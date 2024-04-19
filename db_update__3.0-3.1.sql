ec6ca668e1379e7f79fa3f5f42d1fa7ba8473da5
Mario Dietlein <github@mditsa.de>
2024-04-19 19:49:19 +0200

    db_upgrade_3.0-3.1

---
+++ db_update__3.0-3.1.sql
@@ -0,0 +1,7 @@
+ALTER TABLE auth_sessions ADD COLUMN api binary(1) NOT NULL DEFAULT '0';
+CREATE TABLE api_keys (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  apikey varchar(50) NOT NULL,
+  description varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
